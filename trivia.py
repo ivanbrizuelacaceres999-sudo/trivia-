3a0736c7faf649c50da49106eb4623be8d610972
Ivan Brizueala <ivanbrizuelacaceres999@gmail.com>
2026-03-11 17:20:45 -0300

    demas preguntas y terminacion

--- trivia.py
+++ trivia.py
@@ -4,3 +4,13 @@ nombre= input('Ingrese su nombre')
 print('el planeta es redondo ?')
 if input('SI/NO') == 'SI':
     puntos = puntos+1
+print('las sandias son redondas ?')
+if input('SI/NO') == 'NO':
+    puntos = puntos+1
+print('Santa clos existe ?')
+if input('SI/NO') == 'SI':
+    puntos = puntos+1
+print('Cerro es mejor q olimpia ?')
+if input('SI/NO') == 'NO':
+    puntos = puntos+1
+print(f'terminas la encuentas {nombre} hiciste {puntos}')
